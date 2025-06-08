b0fab5ef9ceae05850b23f030efbfd57e294b4b9
Stefanov-D <email: dob.stefanovd@gmail.com>
2025-06-08 03:19:51 +0200

    EditListingImagesViewAndController

--- RealEstate/Controllers/ListingController.cs
+++ RealEstate/Controllers/ListingController.cs
@@ -60,16 +60,22 @@ namespace RealEstate.Controllers
         {
             if (!ModelState.IsValid)
             {
+                // If validation fails, make sure to repopulate dropdowns
+                ViewBag.Categories = new SelectList(await db.Categories.ToListAsync(), "Id", "Name");
+                ViewBag.ListingTypes = new SelectList(await db.ListingTypes.OrderBy(t => t.Id).ToListAsync(), "Id", "Name");
+
+                // Return view with current model (including uploaded images)
                 return View(obj);
             }
+
             ViewBag.Categories = new SelectList(await db.Categories.ToListAsync(), "Id", "Name");
             ViewBag.ListingTypes = new SelectList(await db.ListingTypes.OrderBy(t => t.Id).ToListAsync(), "Id", "Name");
 
-            Listing? listingToUpdate = await propertyService.GetListingByIdAsync(id);
-
+            // Fetch the listing entity to update
+            var listingToUpdate = await propertyService.GetListingByIdAsync(id);
             if (listingToUpdate == null)
             {
-                return View(obj);
+                return NotFound();
             }
 
             var success = await propertyService.UpdateListingAsync(obj, listingToUpdate);
@@ -79,14 +85,66 @@ namespace RealEstate.Controllers
                 return View(obj);
             }
 
-            return View(obj);
+            // After successful update, reload the model from database including images
+            var updatedListing = await propertyService.GetListingViewModelByIdAsync(id);
+
+            if (updatedListing == null)
+            {
+                return NotFound();
+            }
+
+            var inputModel = new UpdateListingInputModel
+            {
+                Title = updatedListing.Title,
+                Description = updatedListing.Description,
+                Price = updatedListing.Price,
+                City = updatedListing.City,
+                ZipCode = updatedListing.ZipCode,
+                Street = updatedListing.Street,
+                CategoryId = updatedListing.CategoryId,
+                ListingTypeId = updatedListing.ListingTypeId,
+                UploadedImagePaths = updatedListing.Images // <-- Make sure this is populated with the image URLs!
+            };
+
+            return View(inputModel);
         }
+
 
         public async Task<IActionResult> Delete(Guid id)
         {
             var listing = await propertyService.GetListingViewModelByIdAsync(id);
 
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadTempImage(List<IFormFile> images)
+        {
+            var wwwrootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+            var tempPath = Path.Combine(wwwrootPath, "uploads", "temp");
+
+            if (!Directory.Exists(tempPath))
+                Directory.CreateDirectory(tempPath);
+
+            var savedPaths = new List<string>();
+
+            foreach (var file in images)
+            {
+                if (file.Length > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    var filePath = Path.Combine(tempPath, fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+
+                    savedPaths.Add("/uploads/temp/" + fileName);
+                }
+            }
+
+            return Json(new { success = true, paths = savedPaths });
         }
     }
 }
