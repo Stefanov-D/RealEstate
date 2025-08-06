8f07d07b8e3babb5950259eda6b0a54bd19af02e
Stefanov-D <email: dob.stefanovd@gmail.com>
2025-08-06 17:23:58 +0300

    AddAreaMapControllerRouteInProgramCs

--- RealEstate/Program.cs
+++ RealEstate/Program.cs
@@ -56,8 +56,12 @@ namespace RealEstate
             app.UseAuthorization();
 
             app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");            
 
             app.MapRazorPages();
 
